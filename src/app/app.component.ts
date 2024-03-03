3d4a6f40fba7ab0bb40d32f4cab5b5f3e9f60fc3
danukaem <danukaem@gmail.com>
2024-03-03 23:37:59 +0800

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
+import { getDatabase, ref, get, set, update, onValue ,child, push ,remove} from 'firebase/database';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.writeData();
+  }
+
+  writeData() {
+    const app = initializeApp(environment.firebase);
+    const db = getDatabase(app);
+    const dataRef = ref(db, 'users/');
+
+
+    console.log('aaaaaaaa1');
+
+    // get(dataRef).then((snapshot) => {
+    //   if (snapshot.exists()) {
+    //     const data = snapshot.val();
+    //     console.log(data);
+    //   } else {
+    //     console.log('No data available');
+    //   }
+    // });
+
+
+    // set(ref(db,'users/'+0),{
+    //   name:'malawana',
+    //   age:33
+    // });
+
+
+    // update(ref(db,'users/'+0),{
+    //   name:'daunka eranda malawana',
+    //   age:33
+    // })
+
+    // set(ref(db,'users/'+0),{
+    //   name:'danuka',
+    //   age:33,
+    //   location:'sri lanka'
+    // })
+
+
+
+    console.log('aaaaa2');
+
+    ////////////////////////////////////////////////////////////
+    // set(ref(db, 'users/boys/school/' + 0), {
+    //   username: 'name',
+    //   email: 'email',
+    //   profile_picture : 'imageUrl'
+    // });
+    
+    // get(child(dataRef, `boys/school`)).then((snapshot) => {
+    //   if (snapshot.exists()) {
+    //     console.log(snapshot.val());
+    //   } else {
+    //     console.log("No data available");
+    //   }
+    // }).catch((error) => {
+    //   console.error(error);
+    // });
+
+
+
+    set(push(ref(db, 'users' )), {
+      date: new Date().toDateString(),
+      time:new Date().toTimeString()
+    });
+
+
+// remove???????
+    ////////////////////////////////////////////////////////////
+
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD42PB50okZuTSiJU0KfUtCVVfioIRkT6w",
+        authDomain: "shoppingapp-1c92f.firebaseapp.com",
+        projectId: "shoppingapp-1c92f",
+        storageBucket: "shoppingapp-1c92f.appspot.com",
+        messagingSenderId: "492583769945",
+        appId: "1:492583769945:web:b3e91d36df449743ae1e48"
+    }
+};
+
